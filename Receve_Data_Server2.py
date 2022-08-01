194e741b2445bf5786ea651503459deba7b2b481
SungJungho1 <s214q@naver.com>
2022-08-01 11:16:55 +0900

    sdsd

--- Receve_Data_Server2.py
+++ Receve_Data_Server2.py
@@ -69,4 +69,4 @@ if __name__ == '__main__':
 
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
     ssl_context.load_cert_chain(certfile='certificate.pem', keyfile='private.key')
-    app.run(host="0.0.0.0", port=443, ssl_context=ssl_context)
+    app.run(host="0.0.0.0", port=80)
