84eca6652cd220adec744f0998f6c389a1324c78
SungJungho1 <s214q@naver.com>
2022-08-01 10:51:06 +0900

    sdsd

--- Receve_Data_Server.py
+++ Receve_Data_Server.py
@@ -2,11 +2,38 @@ from flask import Flask, request
 from Get_yogiyo import *
 from flask_cors import CORS
 import json
+import ssl
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
-
+key = """-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAwXqtznqQcUI7ISyqGHfwkCg9Vky1bPxqOSZWruwJl+Q3NwYn
+MCwPlMRJ81Y+A2ms9Yssg2SrxOWVm4zzhZlrg5Sq8ElyqtB99ODXl/Q5v1YGlyr4
+0y7+LkBgHzDvX15w8Jy6pwZhhp9VLgHHDK97ST2RR9vnIgrgGP4m35Ax8R53xlVm
+vO9tDHffxr/THHTZ/FsCqlX2n05rkU+aL57GSNocBTzWH3eEIH4y1vFdjzUzc0Da
+oF8zbDIbDOUp5YYHH/x2cVMcRZjMZI+WpHYhzfjOaZ5Skkk1Co/eBZKUE/tUXkN4
+87QaxMBPwnCJIeArWHhdNJmWcsqad95IcHg/GwIDAQABAoIBADA5cr7nX79LIc/Y
+ASGOK6DS1s7+2a2rkJJkEvEQlieM05Jcb0IbiVqsPj5vvY8+NMUTBmpNml6+Vrbp
+v/0Jy3mFgcHYnMMkqnBx5mrSjd46A/SD2ndQ3DwNAahkmxVrRb1DY2akOYTDjAim
+msV3q8QKm36MuiSLD2ZLWscYeso9PedVjlfdk/KAZb7ZAtQPrS/6SLLV5FwYDOig
+sQrD7FwT2XkTgsVTlXs+FfHi/thHkoSTjGDkPbFtxfhiJRDrkwfqs5gVZFuvM4Rk
+fYQcGd3ajFWXznZFWfYT6vnHV7mGbGL6ZsOm7TPqUmayEH+JUo2bsd/XCHiny6WD
+pEi+HPECgYEA40YqYcHG4XY5R1tYtQQPoGW8zdtOddUZXX4D5utoZqxkVfLobAKI
+aDGmZH9dXsBRICER74/oiDtiUbsa4JLgbjHsEDpU+nJIj6nYTrHtR+1TrXGMDh0s
+929mq9N4JRVgjqDKBg17LyI4UZv/LituaWmWQ56XzIq9BwL4XoFrWwMCgYEA2e8G
+Dfhatr74Za+USfmztK4+aMJTaiDpzHJWEIXItyd7YCiZh6QxP1alArPG0swYRBbt
+SP6eh161D0K2SDWIvwlGTXDigulW3P8LkNfyBn78s0ODdudsgiHo2LbOwwt8CpCP
+6SCGy9bR7X2ePIzBjqvp1ZPMd9Z3Cv8uomrCBAkCgYEA4IN++KU5d56XhOM7JU7G
+tRfrxJxBzOtfrFkBN8wTjWH9mF5mtdRmti9eBWdAcIqhWZDxq+tItdOcE8s3CORv
+asxMO9ez/NiKz8jkExF3qpdLE0rZJVQzCJf3IAS+ajvM7/jsub/1kaFHa28kTZK/
+9shElbYElR31EAkiHBHTYMsCgYB6/4gvQkgQc9/FpD7RMXjXNLBMd8CUWVca6Dzi
+vl713/al4sQj5etVkBU5XTWNq1rWswng9LOMO0t8/W3beztedmGTO27b6832lIt2
+iCo0uF7u3d/Ir/xxxamhBivTsRuk6soODSmGygtZOligW+CyIRysSepQiF5Peo5b
+ZF1z4QKBgQDcpzF/3NOA6iN3o85/uodM5r7O2CqabFb160g7ra3SoS0ySK2Bf9xx
+TKKSpBCVQ8NkgieZZnM0MBumzCZc/futkd0vKQAdrd9hwKXMTGFnx1GAAY7Tg7hz
+G1dJEuEzMaeuhfOBF2JFqyKIA/1TSufXwkeHE7wEITeUdFu2S/RkGw==
+-----END RSA PRIVATE KEY-----"""
 
 @app.route('/getStores')
 def getStores():
@@ -67,4 +94,7 @@ def translate():
 
 if __name__ == '__main__':
 
-    app.run(host="0.0.0.0", debug=True, port=80)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='certificate.pem', keyfile='private.key')
+    app.run(host="0.0.0.0", port=443, ssl_context=ssl_context)
+    # app.run(host="0.0.0.0", debug=True, port=80)
