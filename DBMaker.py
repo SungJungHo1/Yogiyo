c35ca9622fcb4b41999d187e5075d89826e2363b
SungJungho1 <s214q@naver.com>
2022-08-16 21:50:17 +0900

    sdsdasdsd

--- DBMaker.py
+++ DBMaker.py
@@ -12,7 +12,7 @@ mycol = mydb['OrderDatas']
 
 def Insert_Data(UserName,UserId,Delivery_Fee,Order_Data,Cart):
     z = randrange(0,900)
-    Order_Code = str(datetime.now().hour) + str(datetime.now().microsecond) + str(z)[-1]
+    Order_Code = str(datetime.now().hour) + str(datetime.now().month) + str(datetime.now().year) + str(int(datetime.now().microsecond / 1000)) + str(z)[-1]
     mycol.insert_one({"Order_Code":Order_Code,"UserName":UserName, "UserId":UserId,"delivery_fee":Delivery_Fee,"Order_Data":Order_Data,"Cart":Cart})
     return Order_Code
 
@@ -21,7 +21,7 @@ def Drop_Users():
 
 if __name__ == "__main__":
     # Insert_Data("Uad859360a7e2589c8c213b3b47fc27a2",'크턱',orderdata,cart)
-    Drop_Users()
+    # Drop_Users()
     x = mycol.find()
     for i in x:
         print(i)
