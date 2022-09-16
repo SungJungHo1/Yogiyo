0f79af143510275b76a1dd9b03e2af4918545a92
SungJungho1 <s214q@naver.com>
2022-09-16 18:36:27 +0900

    9/16#3

--- DBMaker.py
+++ DBMaker.py
@@ -21,7 +21,7 @@ def Insert_Data(UserName, UserId, Delivery_Fee, Order_Data, Cart):
     datetime_utc2 = datetime.now(timezone_kst)
 
     mycol.insert_one({"Order_Code": Order_Code, "UserName": UserName, "UserId": UserId,
-                     "delivery_fee": Delivery_Fee, "Order_Data": Order_Data, "Cart": Cart, "Order_End": True, 'Del_End': False, "Memo": "음식 문앞에두고 꼭 전화한번 주세요!", "Rider": "", "Order_Time": datetime_utc2})
+                     "delivery_fee": Delivery_Fee, "Order_Data": Order_Data, "Cart": Cart, "Order_End": True, 'Del_End': False, "Memo": "음식 문앞에두고 꼭 전화한번 주세요!", "Rider": "", "Order_Time": str(datetime_utc2)})
     return Order_Code
 
 
@@ -29,7 +29,7 @@ def Insert_Err(Errors):
     timezone_kst = timezone(timedelta(hours=9))
     datetime_utc2 = datetime.now(timezone_kst)
 
-    errcol.insert_one({"Errors": Errors, "Time": datetime_utc2})
+    errcol.insert_one({"Errors": Errors, "Time": str(datetime_utc2)})
 
 
 def Edit_Data(Code, Ur):
