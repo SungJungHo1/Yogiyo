3137cbcd4e1cc7fda1751495ee4a6bd78341890c
SungJungho1 <s214q@naver.com>
2022-09-21 01:02:11 +0900

    9/20#13

--- Make_Datas.py
+++ Make_Datas.py
@@ -429,7 +429,7 @@ def Make_DD(userId, Total_pay, deliver_fee, Total_Count, UserName, Order_Code):
         "messages": [
             {
                 "type": "flex",
-                # "altText": "ส่งคำสั่งซื้อของคุณเรียบร้อยแล้ว!",
+                "altText": "ส่งคำสั่งซื้อของคุณเรียบร้อยแล้ว!",
                 "contents": {
                     "type": "carousel",
                     "contents": [
